ef5f2c4dda5125f5558eea131687e927002fe065
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-09-21 19:50:43 +0300

    Create simple API for bookmark CRUD operations.

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -2,30 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
 class BookmarkController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function index()
     {
+        $bookmarks = app('db')->select("SELECT * FROM bookmark");
 
+        return response()->json($bookmarks);
     }
 
-    public function store()
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request)
     {
+        $data = json_decode($request->getContent(), true);
 
+        app('db')->insert(
+            'insert into bookmark (title, url, created_at) values (?, ?, ?)',
+            [$data['title'], $data['url'], new \DateTime()]
+        );
+
+        return response()->json([], 201);
     }
 
-    public function show()
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show(int $id)
     {
+        $bookmark = app('db')->select(
+            "SELECT * FROM bookmark WHERE id = ? LIMIT 1",
+            [$id]
+        );
 
+        return response()->json($bookmark);
     }
 
-    public function update()
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, int $id)
     {
+        $data = json_decode($request->getContent(), true);
 
+        app('db')->update(
+            'update bookmark (title, url, updated_at) values (?, ?, ?) where id = ?',
+            [$id, $data['title'], $data['url'], new \DateTime()]
+        );
+
+        return response()->json([], 204);
     }
 
-    public function destroy()
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy(int $id)
     {
+        app('db')->delete('delete from bookmark where id = ?', [$id]);
 
+        return response()->json([], 204);
     }
 }
