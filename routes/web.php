777b57bcb153ef4aab58575f83a9046745102bef
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-09-21 23:44:30 +0300

    Return default route.

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
 $router->group(['prefix' => 'bookmark'], function () use ($router) {
     $router->get('/', ['uses' => 'BookmarkController@index']);
     $router->post('/', ['uses' => 'BookmarkController@store']);
